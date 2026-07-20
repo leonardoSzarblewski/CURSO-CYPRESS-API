4233423abd84cb232f28f6f124e523526a9d2820
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-20 10:55:32 -0300

    refactor: adjust error text

--- cypress/e2e/get-api.cy.js
+++ cypress/e2e/get-api.cy.js
@@ -1,29 +1,27 @@
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
 
-describe('Buscar dispositivos', () => {
+describe("Buscar dispositivos", () => {
+  it("Busca um dispositivo especifico", () => {
+    const get_id = "7";
 
-    it('Busca um dispositivo especifico', () => {
-        const get_id = '7'
+    cy.buscaDispositivo(get_id).then((response) => {
+      expect(response.status).equal(200);
+      expect(response.body).not.empty;
+      expect(response.body.id).equal(get_id);
 
-        cy.buscaDispositivo(get_id).then((response) => {
-            expect(response.status).equal(200)
-            expect(response.body).not.empty
-            expect(response.body.id).equal(get_id)
-
-            expect(response.body.name).equal('Apple MacBook Pro 16')
-            expect(response.body.data['CPU model']).equal('Intel Core i9')
-        })
-        
+      expect(response.body.name).equal("Apple MacBook Pro 16");
+      expect(response.body.data["CPU model"]).equal("Intel Core i9");
     });
+  });
 
-    it('Busca um dispositivo inexistente', () => {
-        const get_id = faker.number.int({ min: 20, max: 999 })
+  it("Busca um dispositivo inexistente", () => {
+    const get_id = faker.number.int({ min: 20, max: 999 });
 
-        cy.buscaDispositivo(get_id).then((response) => {
-            expect(response.status).equal(404)
-            expect(response.body.error).equal(`Oject with id=${get_id} was not found.`)
-    
-        })
-        
+    cy.buscaDispositivo(get_id).then((response) => {
+      expect(response.status).equal(404);
+      expect(response.body.error).equal(
+        `Object with id=${get_id} was not found.`,
+      );
     });
+  });
 });
