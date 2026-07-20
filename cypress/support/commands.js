6742b90431461cc18e97d8bf7aaa5aabcb4e2b96
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-20 13:29:35 -0300

    refactor: reorganize commands in commands.js and add user registration command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,29 +1,39 @@
-Cypress.Commands.add('cadastraDispositivo', (payload) => {
-    cy.request({
-        method: 'POST',
-        url: '/objects',
-        failOnStatusCode: false,
-        body: payload 
+import { faker } from "@faker-js/faker";
 
-    })
-})
+// Deve registrar um usuário
+Cypress.Commands.add("registraUsuario", (payload) => {
+  cy.request({
+    method: "POST",
+    url: "/register",
+    failOnStatusCode: false,
+    headers: {
+      "x-api-key": Cypress.env("API_TOKEN"),
+    },
+    body: payload,
+  });
+});
 
-Cypress.Commands.add('buscaDispositivo', (get_id) => {
-    cy.request({
-        method: 'GET',
-        url: `/objects/${get_id}`,
-        failOnStatusCode: false
+Cypress.Commands.add("cadastraDispositivo", (payload) => {
+  cy.request({
+    method: "POST",
+    url: "/objects",
+    failOnStatusCode: false,
+    body: payload,
+  });
+});
 
-    })
-})
+Cypress.Commands.add("buscaDispositivo", (get_id) => {
+  cy.request({
+    method: "GET",
+    url: `/objects/${get_id}`,
+    failOnStatusCode: false,
+  });
+});
 
-Cypress.Commands.add('deletaDispositivo', (delete_id) => {
-    cy.request({
-        method: 'DELETE',
-        url: `/objects/${delete_id}`,
-        failOnStatusCode: false
-
-    })
-})
-
-
+Cypress.Commands.add("deletaDispositivo", (delete_id) => {
+  cy.request({
+    method: "DELETE",
+    url: `/objects/${delete_id}`,
+    failOnStatusCode: false,
+  });
+});
