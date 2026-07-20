7c0bd38a12ec494ef10b72780489cb35b9767f30
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-20 11:08:39 -0300

    refactor: standardize quotes and improve formatting in post-api tests

--- cypress/e2e/post-api.cy.js
+++ cypress/e2e/post-api.cy.js
@@ -1,23 +1,19 @@
-describe('Cadastro de dispositivos', () => {
+describe("Cadastro de dispositivos", () => {
+  const body = require("../fixtures/cadastraDevice.json");
 
-    const body = require('../fixtures/cadastraDevice.json')
+  it("Cadastra um dispositivo", () => {
+    const dataAtual = new Date().toISOString().slice(0, 10);
 
-    it('Cadastra um dispositivo', () => {
-        const dataAtual = new Date().toISOString().slice(0, 10);
-
-            cy.cadastraDispositivo(body).then((response) => {
-                expect(response.status).equal(200)
-                expect(response.body.id).not.empty
-                expect(response.body.createdAt.slice(0, 10)).equal(dataAtual)
-                expect(response.body.data.owner).equal('Leonardo Szarblewski')
-
-            })
-
+    cy.cadastraDispositivo(body).then((response) => {
+      expect(response.status).equal(200);
+      expect(response.body.id).not.empty;
+      expect(response.body.data.owner).equal("Leonardo Szarblewski");
     });
+  });
 
-    it('Cadastra um dispositivo sem body', () => {
-        cy.cadastraDispositivo('').then((response) => {
-          expect(response.status).equal(400)  
-        })
+  it("Cadastra um dispositivo sem body", () => {
+    cy.cadastraDispositivo("").then((response) => {
+      expect(response.status).equal(400);
     });
+  });
 });
